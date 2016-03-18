ce494f1e65424f2f149b16abc40fab5a16bab288
Brian Silverman <bri@raspberrystem.com>
2016-03-18 13:00:17 -0400

    fritzing - script to generated images that don't have breadboards

--- fritzing/diagrams/build.sh
+++ fritzing/diagrams/build.sh
@@ -1,44 +1,40 @@
-#LED_Matrix_Close.fzz special
+./double_fzz_to_jpg Buttons_with_GPIO_1.fzz                     40  30
+./double_fzz_to_jpg Buttons_with_GPIO_2.fzz                     40  30
+./double_fzz_to_jpg Flashlight.fzz                              40  30
+./double_fzz_to_jpg Introducing_Buttons_1.fzz                   40  30
+./double_fzz_to_jpg Introducing_Buttons_2.fzz                   40  30
+./double_fzz_to_jpg Introducing_Buttons_3.fzz                   40  30
+./double_fzz_to_jpg Introducing_Buttons_4.fzz                   40  30
+./double_fzz_to_jpg Just_Power.fzz                              40  30
+./double_fzz_to_jpg LED_Matrix_Accel_Buttons.fzz
+./double_fzz_to_jpg LED_Matrix_Accel.fzz
+./double_fzz_to_jpg LED_Matrix.fzz                              40  30
+./double_fzz_to_jpg LED_Matrix_power_only.fzz                   40  20
+./double_fzz_to_jpg LED_Matrix_unconnected.fzz                  40  30
+./double_fzz_to_jpg Many_Buttons.fzz                            40  30
+./double_fzz_to_jpg Minecraft_1button.fzz                       40  30
+./double_fzz_to_jpg Minecraft_4way_2buttons.fzz
+./double_fzz_to_jpg Minecraft_Accel_4way_1button.fzz
+./double_fzz_to_jpg Minecraft_Matrix_4way_2buttons.fzz
+./double_fzz_to_jpg Minecraft_Matrix_Accel_4way_1button.fzz
+./double_fzz_to_jpg Minecraft_Matrix_Accel_4way_2buttons.fzz
+./double_fzz_to_jpg Minecraft_Matrix_updown_1button.fzz
+./double_fzz_to_jpg Your_First_Breadboard_Add_GPIO.fzz          40  30
+./double_fzz_to_jpg Your_First_Breadboard_Add_Power.fzz         40  30
+./double_fzz_to_jpg Your_First_Breadboard_Battery_Only.fzz      40  10
+./double_fzz_to_jpg Your_First_Breadboard_LED_Added.fzz         40  10
+./double_fzz_to_jpg Your_First_Breadboard_Remove_Battery.fzz    40  10
+./double_fzz_to_jpg Your_First_Breadboard_Resistor_Added.fzz    40  10
+./double_fzz_to_jpg Your_First_Breadboard.fzz                   40  10
+./double_fzz_to_jpg simon-0but-0led.fzz
+./double_fzz_to_jpg simon-1but-0led.fzz
+./double_fzz_to_jpg simon-2but-0led.fzz
+./double_fzz_to_jpg simon-3but-0led.fzz
+./double_fzz_to_jpg simon-4but-0led.fzz
+./double_fzz_to_jpg simon-4but-1led.fzz
+./double_fzz_to_jpg simon-4but-2led.fzz
+./double_fzz_to_jpg simon-4but-3led.fzz
+./double_fzz_to_jpg simon-4but-4led.fzz
 
-function fzz_to_jpg() {
-    echo "$1"
-    sh ./fzz_to_jpg.sh "$1" $2 $3
-}
-
-fzz_to_jpg  Buttons_with_GPIO_1.fzz                     40  30
-fzz_to_jpg  Buttons_with_GPIO_2.fzz                     40  30
-fzz_to_jpg  Flashlight.fzz                              40  30
-fzz_to_jpg  Introducing_Buttons_1.fzz                   40  30
-fzz_to_jpg  Introducing_Buttons_2.fzz                   40  30
-fzz_to_jpg  Introducing_Buttons_3.fzz                   40  30
-fzz_to_jpg  Introducing_Buttons_4.fzz                   40  30
-fzz_to_jpg  Just_Power.fzz                              40  30
-fzz_to_jpg  LED_Matrix_Accel_Buttons.fzz
-fzz_to_jpg  LED_Matrix_Accel.fzz
-fzz_to_jpg  LED_Matrix.fzz                              40  30
-fzz_to_jpg  LED_Matrix_power_only.fzz                   40  20
-fzz_to_jpg  LED_Matrix_unconnected.fzz                  40  30
-fzz_to_jpg  Many_Buttons.fzz                            40  30
-fzz_to_jpg  Minecraft_1button.fzz                       40  30
-fzz_to_jpg  Minecraft_4way_2buttons.fzz
-fzz_to_jpg  Minecraft_Accel_4way_1button.fzz
-fzz_to_jpg  Minecraft_Matrix_4way_2buttons.fzz
-fzz_to_jpg  Minecraft_Matrix_Accel_4way_1button.fzz
-fzz_to_jpg  Minecraft_Matrix_Accel_4way_2buttons.fzz
-fzz_to_jpg  Minecraft_Matrix_updown_1button.fzz
-fzz_to_jpg  Your_First_Breadboard_Add_GPIO.fzz          40  30
-fzz_to_jpg  Your_First_Breadboard_Add_Power.fzz         40  30
-fzz_to_jpg  Your_First_Breadboard_Battery_Only.fzz      40  10
-fzz_to_jpg  Your_First_Breadboard_LED_Added.fzz         40  10
-fzz_to_jpg  Your_First_Breadboard_Remove_Battery.fzz    40  10
-fzz_to_jpg  Your_First_Breadboard_Resistor_Added.fzz    40  10
-fzz_to_jpg  Your_First_Breadboard.fzz                   40  10
-fzz_to_jpg  simon-0but-0led.fzz
-fzz_to_jpg  simon-1but-0led.fzz
-fzz_to_jpg  simon-2but-0led.fzz
-fzz_to_jpg  simon-3but-0led.fzz
-fzz_to_jpg  simon-4but-0led.fzz
-fzz_to_jpg  simon-4but-1led.fzz
-fzz_to_jpg  simon-4but-2led.fzz
-fzz_to_jpg  simon-4but-3led.fzz
-fzz_to_jpg  simon-4but-4led.fzz
+./simple_fzz_to_jpg Your_First_Circuit_complete.fzz
+./simple_fzz_to_jpg Your_First_Circuit_connect_battery.fzz
